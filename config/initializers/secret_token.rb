be255ea327a5e2850ff998d919f2a534ab2b74ba
Matthew A Rumph <matthew.rumph@gmail.com>
2014-01-06 17:15:10 -0500

    Initializing repository for picture_blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PictureBlog::Application.config.secret_key_base = '5ce16b2fc95683651b363281a24f82e6f0a01cc243767a5bf19c45c12c13cd5fd2948ce03acd87c0a556e6cf01066de81089ebf905f2631813ea2b51f29dcf3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PictureBlog::Application.config.session_store :cookie_store, key: '_picture_blog_session'
